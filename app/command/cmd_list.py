8a3270a4f21a5125af89590c218dfaff04629d7f
DESKTOP-3DMF6CO\zhuny <zhuny936772@gmail.com>
2025-04-10 02:01:21 +0900

    video list

--- app/command/cmd_list.py
+++ app/command/cmd_list.py
@@ -1,4 +1,14 @@
+from app.core.models import VideoOutput
+
+
 def run():
     """
     생성한 비디오의 ID와 리스트를 출력합니다.
     """
+    for i in range(1000):
+        video = VideoOutput(i)
+        if not video.is_exists():
+            break
+
+        video.load()
+        print(f'{i}. {video.name}')
