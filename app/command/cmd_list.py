daddff7e3161276fb26cccf8ab90def163a39e8d
DESKTOP-3DMF6CO\zhuny <zhuny936772@gmail.com>
2025-05-08 00:03:02 +0900

    load post from api and list of file

--- app/command/cmd_list.py
+++ app/command/cmd_list.py
@@ -1,14 +1,17 @@
+from app.core.model.custom import IntList
 from app.core.model.video import VideoOutput
 
 
-def run():
+def run(video_id_list: IntList):
     """
     생성한 비디오의 ID와 리스트를 출력합니다.
+
+    :param video_id_list: 비디오 리스트
     """
-    for i in range(1000):
+    for i in video_id_list:
         video = VideoOutput(i)
         if not video.is_exists():
-            break
+            continue
 
         video.load()
         print(f'{i}. {video.model.name}')
